170ec2da6ae265570d273dcab2504538d4ab1f2b
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2015-05-10 11:56:33 +1000

    Initial Commit

---
+++ JsonSwiftson/JsonSwiftson.h
@@ -0,0 +1,19 @@
+//
+//  JsonSwiftson.h
+//  JsonSwiftson
+//
+//  Created by Evgenii Neumerzhitckii on 10/05/2015.
+//  Copyright (c) 2015 Evgenii Neumerzhitckii. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JsonSwiftson.
+FOUNDATION_EXPORT double JsonSwiftsonVersionNumber;
+
+//! Project version string for JsonSwiftson.
+FOUNDATION_EXPORT const unsigned char JsonSwiftsonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JsonSwiftson/PublicHeader.h>
+
+
