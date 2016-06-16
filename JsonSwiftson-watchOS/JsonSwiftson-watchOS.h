84844535e7d3fe57782a313257e631ed9ee6ea78
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2016-06-17 09:49:52 +1000

    Add support for tvOS and watchOS

---
+++ JsonSwiftson-watchOS/JsonSwiftson-watchOS.h
@@ -0,0 +1,19 @@
+//
+//  JsonSwiftson-watchOS.h
+//  JsonSwiftson-watchOS
+//
+//  Created by Evgenii on 17/06/2016.
+//  Copyright © 2016 Evgenii Neumerzhitckii. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+//! Project version number for JsonSwiftson-watchOS.
+FOUNDATION_EXPORT double JsonSwiftson_watchOSVersionNumber;
+
+//! Project version string for JsonSwiftson-watchOS.
+FOUNDATION_EXPORT const unsigned char JsonSwiftson_watchOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JsonSwiftson_watchOS/PublicHeader.h>
+
+
